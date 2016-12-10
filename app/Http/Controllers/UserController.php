795f5cf67d591519fff59144a9a9d9760b909716
Ashwin Rana <Ashwin Rana>
2016-12-10 17:27:05 +0545

    Social Site Under development

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,138 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+
+class UserController extends Controller
+{
+
+
+
+    public function main()
+    {
+        return view('users.welcome2');
+    }
+
+    function index()
+    {
+        $users = User::paginate(5);
+        return view('users.List', ['users' => $users]);
+    }
+
+
+    function create()
+    {
+        return view('users.create');
+    }
+
+
+    function store( Request $request){
+//        dd( $_FILES );
+//  dd($request->file('image'));
+//        if($request->hasFile('image')){
+//            $image = $request->file('image');
+//            $image->move(public_path('uploads') , time() . "." . $image->getClientOriginalExtension());
+//        }
+//        die;
+        $user = $request->all();
+//        $user['image'] = $image->getClientOriginalName();
+        User::create( $user );
+        return redirect('home');
+//    dd($request->all());
+    }
+
+    function profile($id)
+    {
+        $users = User::find($id);
+
+        return view('users.user')->with('user',$users);
+
+    }
+
+
+    function User($user_name)
+    {
+        $user = User::where('user_name',$user_name);
+        if(!$user){
+            abort(404);
+        }
+        return view('users.user')->with('user',$user);
+
+    }
+
+    function edit($id)
+    {
+        $user = User::find($id);
+        return view('users.edit', ['user' => $user]);
+
+    }
+
+
+    function update(Request $requests, $id)
+    {
+        $user = User::find($id);
+        $user->update($requests->all());
+//        dd($user);
+        if($requests->save(update))
+        return redirect('home');
+
+    }
+
+
+    function delete($id)
+    {
+        $user = User::find($id);
+        $user->delete();
+        return redirect()->route('userList');
+
+    }
+
+
+    function login()
+    {
+        return view('users.login');
+    }
+
+    function home()
+    {
+        return view('users.welcome2');
+    }
+
+
+
+    function postLogin(Request $request)
+    {
+//        dd($request->all());
+        if (Auth::attempt($request->only('user_name', 'password'))) {
+
+//            dd(Auth::user());
+//            dd("True");
+//            return redirect()->intended(route('userList'));
+            return redirect()->intended(route('home'));
+        }
+
+//        dd($request->all());
+        return redirect()->route('login')->with('message', '<div class="alert alert-danger">Incorrect User Name or Password</div>');
+
+    }
+
+
+    function search()
+    {
+
+        return view('users.search');
+    }
+
+
+    function logout(){
+
+        Auth::logout();
+        return redirect()->route('login')->with('msg',"Thanks for stoppong by");
+    }
+}
---
+++ resources/views/users/welcome2.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="Flix Buzz is a social website where u are free to do anything">
+    <meta name="author" content="Ashwin Rana">
+    <link rel="icon" href="{{URL::to('src/img/logo/logo2.png')}}">
+
+    <title>flix - Home</title>
+
+    <!-- Bootstrap core CSS -->
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="{{ url('/demo') }}"> <img src="{{URL::to('src/img/logo/logo2.png')}}"></a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+            <div class="navbar-form navbar-right">
+                <a href="{{ url('/login') }}"><button class="btn btn-success">Sign In</button></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <a href="{{ url('/create') }}"><button class="btn btn-success">Sign Up</button></a>
+                </div>
+        </div><!--/.navbar-collapse -->
+    </div>
+</nav>
+</body>
+</html>
